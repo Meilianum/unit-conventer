db689c85b298ad29e40c6e50a128b7b106bdc405
Rukhsana jahan <164498186+Meilianum@users.noreply.github.com>
2024-10-16 21:06:32 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,10 @@
+import UnitConverterComponent from "@/components/ui/unit";
+export default function Home() {
+  return(
+    <div>
+      <h1 className="text-3xl font-bold underline boader-2 p-4 text-center bg-lime-400">Unit Converter</h1>
+    <UnitConverterComponent />
+
+    </div>
+  )
+}
